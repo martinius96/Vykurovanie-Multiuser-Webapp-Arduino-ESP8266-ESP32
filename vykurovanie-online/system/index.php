7174c3dc4a1340b437dbc3856aa824ec461e989d
Martin Chlebovec <martinius96@gmail.com>
2021-12-11 18:25:58 +0100

    Update index.php

--- vykurovanie-online/system/index.php
+++ vykurovanie-online/system/index.php
@@ -23,9 +23,34 @@ include ("menu.php");
   <div class="container">
       <div class="row">
         <div class="col-lg-12">									
-<?php //<div class="alert alert-danger">
-//<strong>Dáta na tento stránke obnovuje na pozadí AJAX každých 15 sekúnd. Zobrazia sa automaticky.</strong>
-//</div>  ?>
+<?php 
+if(isset($_POST['zapis_hodnoty'])){
+$vsetko_get = mysqli_query($con,"SELECT * FROM `user_vykurovanie` WHERE `id`='".$_SESSION['uid']."'") or die(mysqli_error($con));
+$vsetko = mysqli_fetch_assoc($vsetko_get);
+$teplota1 = mysqli_real_escape_string($con, $_POST["teplomer1"]);
+$teplota2 = mysqli_real_escape_string($con, $_POST["teplomer2"]);
+$teplota3  = mysqli_real_escape_string($con, $_POST["teplomer3"]);
+$teplota4  = mysqli_real_escape_string($con, $_POST["teplomer4"]);
+$teplota5  = mysqli_real_escape_string($con, $_POST["teplomer5"]);
+$teplota6  = mysqli_real_escape_string($con, $_POST["teplomer6"]);
+$kodik = $vsetko['code'];
+$ins = mysqli_query($con,"INSERT INTO `data_vykurovanie` (`teplota1`,`teplota2`,`teplota3`,`teplota4`,`teplota5`,`teplota6`,`code`) VALUES ('$teplota1', '$teplota2', '$teplota3', '$teplota4', '$teplota5', '$teplota6', '$kodik')") or die (mysqli_error($con));
+?>
+<div class="alert alert-info">
+<b>Teploty zapísané!</b>
+</div>
+<?php
+}
+?>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"  method="post">
+			<input  type="number" min=-30 max=75 step=0.5 name="teplomer1" required><br> 
+			<input  type="number" min=-30 max=75 step=0.5 name="teplomer2" required><br> 
+			<input  type="number" min=-30 max=75 step=0.5 name="teplomer3" required> <br>
+			<input  type="number" min=-30 max=75 step=0.5 name="teplomer4" required> <br>
+			<input  type="number" min=-30 max=75 step=0.5 name="teplomer5" required> <br>
+			<input  type="number" min=-30 max=75 step=0.5 name="teplomer6" required> <br>
+			<input type="submit" class="btn btn-success" name="zapis_hodnoty" value="Zapísať" disabled>
+</form>
 <div class="alert alert-success">
 <strong>Teplota v zelenom rámiku predstavuje riadiacu teplotu pre termostat.</strong>
 </div> 
